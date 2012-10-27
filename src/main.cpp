498662f27668da423f7fc9be11e1bc99422dcd25
admirestator <admirestator@gmail.com>
2012-10-27 12:33:15 +0800

    add check platform

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
-#include "main_window.h"
 #include <QApplication>
+
+#include <iostream>
+
+#include "main_window.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    
+
+#ifdef Q_WS_WIN
+   cout << "Win32 Platform"  << endl;
+#endif
+#ifdef Q_WS_X11
+   cout << "Linux/Unix Platform"  << endl;
+#endif
+#ifdef  Q_WS_MAC
+   cout << "Mac OS Platform"  << endl;
+#endif
     return a.exec();
 }
